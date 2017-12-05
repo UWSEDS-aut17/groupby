a39eb2f56e80b329c4c3af7fab59053880471cf2
jtkovacs <jacobtkovacs@gmail.com>
2017-12-04 18:01:53 -0800

    groupby validate_args() test

--- groupby/groupby.py
+++ groupby/groupby.py
@@ -1,5 +1,5 @@
-"""
-This script 
+"""This script 
+
 """
 
 
@@ -11,40 +11,46 @@ import sys
 #import gcal
 
 
-def validate_args(user_args):    
+def validate_args(user_args):
+    r"""Checking user-provided arguments for validity.
+        
+    Parameters
+    ----------
+    user_args : list of strings
+        arguments passed from command line to script via sys.argv
+        
+    Returns
+    -------
+    msg : str or None
+        error message
+        
+    """
 
     if len (user_args) < 3:
-        print("\n\n Please indicate at least one social media dataset, for example: \n\n\t python groupby.py -T path/Twitter_directory/")
-        return 'Failed to provide minimum argument'          
+        print("\n\n Please indicate at least one social media dataset, " 
+            "for example: \n\t python groupby.py -T path/Twitter_directory/")
+        return "Failed to provide minimum argument"    
 
     options = user_args[1::2]
     soc_med = False
     for opt in options:
         if opt not in ['-T', '-F', '-L', '-C']:
-            print("\n\n Please indicate valid options: \n\n\t -T, -F, -L, or -C")
-            return 'Wrong option provided (not -T, -F, -L or -C)'
+            print("\n\n Please indicate valid options: \n\t -T, -F, -L, or -C")
+            return "Wrong option provided (not -T, -F, -L or -C)"
         if opt in ['-T', '-F', '-L']:
             soc_med = True
     if soc_med == False:
-        print("\n\n Please indicate at least one social media dataset, for example: \n\n\t python groupby.py -T path/Twitter_directory/")
-        return 'Failed to provide social media data'          
+        print("\n\n Please indicate at least one social media dataset, "
+            "for example: \n\t python groupby.py -T path/Twitter_directory/")
+        return "Failed to provide social media data"  
     
-    ## each option has an arg?
     if len(user_args)-1 % 2 != 0:
-        print('\n\n Please make sure every option (-T, -F, -L, -C) has a corresponding path ')
-        return('Incomplete option:argument pair')
+        print("\n\n For every option (-T, -F, -L, -C), indicate a corresponding "
+            "path, for example: \n\t python groupby.py -T path/here/")
+        return "Incomplete option:argument pair"
+    
+    return None
 
-
-
-"""
- 
-    # at least two arguments, the first of which indicates a social media dataset?
-    if len(user_args) < 3 or user_args[1] not in ['-T', '-F', '-L']:
-        print("\n\n Please indicate at least one social media dataset, for example: \n\n\t python groupby.py -T path/Twitter_directory/")
-        return 'Failed to provide minimum argument'
-"""
-
-"""
 
 
 def open_files(user_args):
@@ -103,42 +109,43 @@ def open_files(user_args):
                 cal = True
 
 
-if tw:
-    unique_tweets,retweeted = tweet_explore(tweets_df)
-    hashtags, hashtags_int, values = hashtag_clean(tweets_df)
-    friends_list, friends_int, m_values = mentions_clean(tweets_df)
-    month_df, labels = date_clean(tweets_df)
-    print('Total number of unique tweets:', unique_tweets)
-    print('Total retweeted tweets', retweeted)
-    plot(tweets,values , tweets_int ,'hashtags' , 'Number' , 'Top 5 Tweet Hashtags', (15,5) , 'Green')
-    plt.show()
-    plot(friends_list,m_values , friends_int ,'Friend' , 'Number of Mentions' , 'Top 5 Friend Mentions', (15,5) , 'Green')
-    plt.show()
-    plot_tweetDate(month_df, labels ,'Month' , 'Number of Tweets' , 'Top Per Month', (15,5) , 'Purple')
-    plt.show()
-        
-if li:
-    con_df_by_week = linkedin.clean_df(con_df, 'Connected On')
-    invites_sent, invites_received = get_sent_receive_invites(invites_df, 'Direction')
-    invites_sent_by_week = clean_df(invites_sent, 'Sent At')
-    invites_received_by_week = clean_df(invites_received, 'Sent At')
-    plot(con_df_by_week,'Connected On','Email Address', 'Weeks', 
-        'Number of Connections', 'Bar Plot - Number of Connections per week', 
-        (15,5), 'purple')
-    plot(invites_sent_by_week,'Sent At','From', 'Weeks', 
-        'Number of Invites', 'Bar Plot - Number of Invites Sent per week', 
-        (15,5), 'green')
-    plot(invites_received_by_week,'Sent At','From', 'Weeks', 
-        'Number of Invites', 'Bar Plot - Number of Invites Sent per week', 
-        (15,5), 'red')
-    import_recruiters_contacts('Connections.csv')
+def analyze_data(user_args):
 
-if fb:
-    pass
+    if tw:
+        unique_tweets,retweeted = tweet_explore(tweets_df)
+        hashtags, hashtags_int, values = hashtag_clean(tweets_df)
+        friends_list, friends_int, m_values = mentions_clean(tweets_df)
+        month_df, labels = date_clean(tweets_df)
+        print('Total number of unique tweets:', unique_tweets)
+        print('Total retweeted tweets', retweeted)
+        plot(tweets,values , tweets_int ,'hashtags' , 'Number' , 'Top 5 Tweet Hashtags', (15,5) , 'Green')
+        plt.show()
+        plot(friends_list,m_values , friends_int ,'Friend' , 'Number of Mentions' , 'Top 5 Friend Mentions', (15,5) , 'Green')
+        plt.show()
+        plot_tweetDate(month_df, labels ,'Month' , 'Number of Tweets' , 'Top Per Month', (15,5) , 'Purple')
+        plt.show()
+            
+    if li:
+        con_df_by_week = linkedin.clean_df(con_df, 'Connected On')
+        invites_sent, invites_received = get_sent_receive_invites(invites_df, 'Direction')
+        invites_sent_by_week = clean_df(invites_sent, 'Sent At')
+        invites_received_by_week = clean_df(invites_received, 'Sent At')
+        plot(con_df_by_week,'Connected On','Email Address', 'Weeks', 
+            'Number of Connections', 'Bar Plot - Number of Connections per week', 
+            (15,5), 'purple')
+        plot(invites_sent_by_week,'Sent At','From', 'Weeks', 
+            'Number of Invites', 'Bar Plot - Number of Invites Sent per week', 
+            (15,5), 'green')
+        plot(invites_received_by_week,'Sent At','From', 'Weeks', 
+            'Number of Invites', 'Bar Plot - Number of Invites Sent per week', 
+            (15,5), 'red')
+        import_recruiters_contacts('Connections.csv')
 
-if cal:
-    pass
-"""
+    if fb:
+        pass
+
+    if cal:
+        pass
 
 
 user_args = sys.argv
