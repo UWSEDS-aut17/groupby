1f21f4462fcf5a415e9cf77ad99124197df7f63c
jtkovacs <jacobtkovacs@gmail.com>
2017-12-04 16:09:51 -0800

    groupby

--- groupby/gcal.py
+++ groupby/gcal.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from pytz import UTC # timezone
 
 cal = Calendar()
-g = open('shsher@uw.edu.ics','rb')
 gcal = Calendar.from_ical(g.read())
 for component in gcal.walk():
     if component.name == "VEVENT":
--- groupby/groupby.py
+++ groupby/groupby.py
@@ -8,45 +8,95 @@ import twitter
 import gcal
 
 
-# check valid args
+## at least two args, first of which is -T, -F, -L?
+print("Please indicate at least one social media dataset, for example: python groupby.py -T path/Twitter_directory/")
 
-# check valid files
+## every other arg is either -T, -F, -L, -C?
+print("Please indicate valid options: -T, -F, -L, or -C")
 
-# get individual    
+## each option has an arg? == files open?
+for i in len(args)-1:
     
-# LINKED IN
-con_df = read_safely('./connections.csv')
-con_df_by_week = clean_df(con_df, 'Connected On')
-invites_df = read_safely('Invitations.csv')
-invites_sent, invites_received = get_sent_receive_invites(invites_df, 'Direction')
-invites_sent_by_week = clean_df(invites_sent, 'Sent At')
-invites_received_by_week = clean_df(invites_received, 'Sent At')
-plot(con_df_by_week,'Connected On','Email Address', 'Weeks', 
-     'Number of Connections', 'Bar Plot - Number of Connections per week', 
-     (15,5), 'purple')
-plot(invites_sent_by_week,'Sent At','From', 'Weeks', 
-     'Number of Invites', 'Bar Plot - Number of Invites Sent per week', 
-     (15,5), 'green')
-plot(invites_received_by_week,'Sent At','From', 'Weeks', 
-     'Number of Invites', 'Bar Plot - Number of Invites Sent per week', 
-     (15,5), 'red')
-import_recruiters_contacts('Connections.csv')
+    # Twitter
+    if args[i] == '-T':       
+        tw_path = args[i+1]
+        tw_file = 'tweets.csv'
+        try:
+            tweets_df = read_twitter(tw_path + tw_file)
+        except:
+            tw = False
+            print("Please provide a valid path to your Twitter directory")
+        else:
+            tw = True
+    
+    # Facebook
+    if args[i] == '-F':
+        fb_path = args[i+1]
+        fb_friends_file = 'html/friends.htm'
+        fb_timeline_file = 'html/timeline.htm'
+        fb_ads_file = 'html/ads.htm'
+        try:
+            fb = open()
+        except:
+            fb = False
+            print("Please provide a valid path to your Facebook directory")
+        else:
+            fb = True
+    
+    # LinkedIn
+    if args[i] == '-L':
+        li_path = args[i+1]
+        li_connections_file = 'connections.csv'
+        li_invitations_file = 'Invitations.csv'
+        try:
+            con_df = linkedin.read_safely(li_path + li_connections_file)
+            invites_df = linkedin.read_safely(li_path + li_invitations_file)
+        except:
+            li = False
+            print("Please provide a valid path to your LinkedIn directory")  
+        else:
+            li = True
 
-# TWITTER
-tweets_df = read_twitter()
-unique_tweets,retweeted = tweet_explore(tweets_df)
-hashtags, hashtags_int, values = hashtag_clean(tweets_df)
-friends_list, friends_int, m_values = mentions_clean(tweets_df)
-month_df, labels = date_clean(tweets_df)
-print('Total number of unique tweets:', unique_tweets)
-print('Total retweeted tweets', retweeted)
-plot(tweets,values , tweets_int ,'hashtags' , 'Number' , 'Top 5 Tweet Hashtags', (15,5) , 'Green')
-plt.show()
-plot(friends_list,m_values , friends_int ,'Friend' , 'Number of Mentions' , 'Top 5 Friend Mentions', (15,5) , 'Green')
-plt.show()
-plot_tweetDate(month_df, labels ,'Month' , 'Number of Tweets' , 'Top Per Month', (15,5) , 'Purple')
-plt.show()
+    if args[i] == '-C':
+        cal_file = args[i+1]
+        try:
+            g = open('shsher@uw.edu.ics','rb')
+        except:
+            cal = False
+            print("Please provide a valid path to your Google Calendar data (ICS file)")  
+        else:
+            cal = True
 
-# FACEBOOK
+if tw:
+    unique_tweets,retweeted = tweet_explore(tweets_df)
+    hashtags, hashtags_int, values = hashtag_clean(tweets_df)
+    friends_list, friends_int, m_values = mentions_clean(tweets_df)
+    month_df, labels = date_clean(tweets_df)
+    print('Total number of unique tweets:', unique_tweets)
+    print('Total retweeted tweets', retweeted)
+    plot(tweets,values , tweets_int ,'hashtags' , 'Number' , 'Top 5 Tweet Hashtags', (15,5) , 'Green')
+    plt.show()
+    plot(friends_list,m_values , friends_int ,'Friend' , 'Number of Mentions' , 'Top 5 Friend Mentions', (15,5) , 'Green')
+    plt.show()
+    plot_tweetDate(month_df, labels ,'Month' , 'Number of Tweets' , 'Top Per Month', (15,5) , 'Purple')
+    plt.show()
+        
+if li:
+    con_df_by_week = linkedin.clean_df(con_df, 'Connected On')
+    invites_sent, invites_received = get_sent_receive_invites(invites_df, 'Direction')
+    invites_sent_by_week = clean_df(invites_sent, 'Sent At')
+    invites_received_by_week = clean_df(invites_received, 'Sent At')
+    plot(con_df_by_week,'Connected On','Email Address', 'Weeks', 
+        'Number of Connections', 'Bar Plot - Number of Connections per week', 
+        (15,5), 'purple')
+    plot(invites_sent_by_week,'Sent At','From', 'Weeks', 
+        'Number of Invites', 'Bar Plot - Number of Invites Sent per week', 
+        (15,5), 'green')
+    plot(invites_received_by_week,'Sent At','From', 'Weeks', 
+        'Number of Invites', 'Bar Plot - Number of Invites Sent per week', 
+        (15,5), 'red')
+    import_recruiters_contacts('Connections.csv')
 
-# CALENDAR
+if fb:
+    
+if cal:
